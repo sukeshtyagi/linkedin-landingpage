076afa38bfe76370c5478ebd628144d231fc9c58
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-26 17:01:44 +0530

    Main Section in landing page set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="landingPageContainer box-border w-screen h-auto overflow-y-auto overflow-x-hidden bg-white">
+    <div className="landingPageContainer box-border w-screen h-auto overflow-y-auto overflow-x-hidden">
       <Header />
       <Hero />
+      <MainSection />
     </div>
   );
 }
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function MainSection() {
+  const [careerSelected, setCareerSelected] = useState(true);
+  const [businessSelected, setBusinessSelected] = useState(false);
+
+  const handleCareerClick = () => {
+    setCareerSelected(true);
+    setBusinessSelected(false);
+  };
+
+  const handleBusinessClick = () => {
+    setCareerSelected(false);
+    setBusinessSelected(true);
+  };
+
+  return (
+    <div className="outerDiv box-border h-auto ">
+      <div className="topDiv box-border h-auto flex items-center justify-center gap-5 pt-2 text-lg mx-10 mt-5 border-b-2 border-gray-400">
+        <button
+          className={`careerButton box-border ${
+            careerSelected ? "text-blue-500 border-b-blue-500 border-b-2" : ""
+          }`}
+          onClick={handleCareerClick}
+        >
+          Premium Career
+        </button>
+
+        <button
+          className={`businessButton box-border ${
+            businessSelected ? "text-blue-500 border-b-blue-500 border-b-2" : ""
+          }`}
+          onClick={handleBusinessClick}
+        >
+          Premium Business
+        </button>
+      </div>
+
+      <div className="midDiv box-border h-auto px-0 overflow-x-hidden overflow-y-auto">
+        {careerSelected && (
+          <h1 className="heading box-border w-3/5 m-auto text-center my-20 text-4xl leading-loose text-black font-normal tracking-wider">
+            Grow professionally with Premium Career
+          </h1>
+        )}
+
+        {businessSelected && (
+          <h1 className="heading box-border w-3/5 m-auto text-center my-20 text-4xl leading-loose text-black font-normal tracking-wider">
+            Get more clients with Premium Business
+          </h1>
+        )}
+
+        <div className="segregationDiv box-border w-1/12 h-3 rounded-xl border-yellow-700 mx-auto bg-yellow-700"></div>
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
