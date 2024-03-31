40ca098d835a60eded3b707f6f7d773e1f9db5ff
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-31 14:35:48 +0530

    Footer css updated

--- src/components/landingPageChildCompoents/Footer.jsx
+++ src/components/landingPageChildCompoents/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="Outer box-border w-3/4 md:w-full h-fit mx-auto mt-4 mb-16">
-      <div className="footer  box-border flex flex-col md:flex-row justify-center items-center md:gap-6 p-2 bg-red-200 ">
+    <div className="Outer box-border w-full  h-fit mx-auto mt-4 mb-16">
+      <div className="footer  box-border flex flex-col  md:flex-row justify-center items-center md:gap-6 p-2">
         <div className="linkedInFooter hidden md:flex mt-4 items-center text-lg md:text-base md:mt-0">
           <p className="title text-lg font-medium text-black tracking-wider">
             <span className="linkedSpan  font-bold text-blue-500 tracking-tight">
